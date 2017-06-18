f05e1423316572682a9182435fab245b81297b6c
limsm9449 <limsm9449@naver.com>
2017-06-18 23:43:05 +0900

    Initial commit

---
+++ app/src/androidTest/java/com/sleepingbear/ennovel/ExampleInstrumentedTest.java
@@ -0,0 +1,11 @@
+package com.sleepingbear.ennovel;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+
+public class ExampleInstrumentedTest extends ApplicationTestCase<Application> {
+    public ExampleInstrumentedTest() {
+        super(Application.class);
+    }
+}
